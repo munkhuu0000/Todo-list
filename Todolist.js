33e2ced0776123b95eff0f04a1f93dbefa20de9e
25LP8703 25LP8703 <25LP8703@pinecones-iMac.local>
2025-10-20 12:10:29 +0800

    todolist1

---
+++ Todolist.js
@@ -0,0 +1,15 @@
+const list = document.querySelector(".list");
+const input = document.querySelector("input");
+const button = document.querySelector(".create-btn");
+
+let content = "";
+
+const listItem = (content) => {
+  return `<div class= "item"> ${content}
+  <button class="delete-btn">Delete</button></div>`;
+};
+button.addEventListener("click", () => {
+  const value = input.value;
+  content += listItem(value);
+  list.innerHTML = value;
+});
